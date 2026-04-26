dddfb60f50065c6b2fe109c5aac29f6cd306f091
Peter Wong <32811464+robertlee22@users.noreply.github.com>
2026-04-26 18:17:01 +0800

    Update d0426t2.py

--- d0426t2.py
+++ d0426t2.py
@@ -20,18 +20,23 @@ class Dps:
         pass
 
     def iter(self, last_dp, e): 
-        con = [each*e  for each in last_dp.con]
-        non = last_dp.non_con
+        
+        con = [each*e  for each in last_dp.con  ] + [e]
+        maxn = max(con)
+        minn = min(con) 
+        con = [maxn] + [minn]
+        non = last_dp.non_con + last_dp.con
         self.con = con 
-        self.non_con = non + [e]
+        self.non_con = non 
+
+        # print(self.con)
         return self 
     
     def get_ans(self): 
        return max(self.con + self.non_con)
-        
 
 s = Solution() 
-r = s.maxProduct([2,3,-2,4])
+r = s.maxProduct([-5,2,4,1,-2,2,-6,3,-1,-1,-1,-2,-3,5,1,-3,-4,2,-4,6,-1,5,-6,1])
 print(r)
 """
 nums = [2,3,-2,4]
