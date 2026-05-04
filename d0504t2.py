09333432455dac35345fe407dbf6c9cd4f64a390
Peter Wong <32811464+robertlee22@users.noreply.github.com>
2026-05-04 13:41:34 +0800

    Update d0504t2.py

--- d0504t2.py
+++ d0504t2.py
@@ -1,5 +1,20 @@
 import bisect 
+from typing import List 
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         return bisect.bisect_left(nums, target) 
+    
+    def searchInsert2(self, nums: List[int], target: int) -> int:
+        lf,rg = 0,len(nums)-1
+        while lf < rg : 
+            mid = (lf + rg) //2 
+            if nums[mid] >= target: 
+                rg = mid
+            else: 
+                lf = mid + 1
+            print(lf, rg)
+        if nums[rg] >= target:
+            return rg 
+        else: 
+            return rg + 1
